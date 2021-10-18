dc8df5a1db2a22869e4b4ee54844f09275aef916
fafa <fafa.afpa@gmail.com>
2021-10-18 17:16:36 +0200

    fichier tp + cprrectoin javascripte

---
+++ JavaScript/tp/exo1.js
@@ -0,0 +1,9 @@
+    
+
+    valeur = prompt("entrez une valeur entiere:","9");
+
+    for (i = 1; i<=9; i ++){
+        multi = i * valeur;
+        document.write("<tr><td>"+ valeur + " x "+ i +"</td><td>" + multi + "</td> </tr>");
+    }
+
